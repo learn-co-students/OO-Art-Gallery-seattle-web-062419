d81b534753532d1e0e0dda1ad56fe9fa357a5990
Jinzi Yan <stephanie.jinzi.yan@gmail.com>
2019-07-01 21:00:21 -0700

    Done.

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,26 @@
 class Painting
 
-  attr_reader :title, :price
-
+  attr_reader :title, :price,:gallery, :artist
   @@all = []
 
-  def initialize(title, price)
+  def initialize(title, price,gallery,artist)
+    @gallery=gallery
+    @artist=artist
     @title = title
     @price = price
+    @@all<<self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.total_price
+    total_price=0
+    @@all.each do |painting|
+      total_price += painting.price
+    end
+    total_price
   end
 
 end
